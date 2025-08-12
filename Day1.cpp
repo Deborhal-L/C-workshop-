72db817f2488f0c42eed64de4d8e1b62febba368
Deborhal L <debo160307@gmail.com>
2025-08-12 10:33:51 +0530

    Create Day1.cpp
    Added c++ code

---
+++ Day1.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "I am deborhal L";
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!\n";
+    cout << "I am deborhal L";
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int x = 50, y = 50, z = 50;
+
+int main() {
+    cout << x + y + z;
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int x, y, z;
+
+int main() {
+    x = y = z = 50; 
+    cout << x + y + z;
+    return 0;
+}
+ #include <iostream>
+using namespace std;
+
+const int myth = 15; // constant value
+int myNum = 10;      // normal variable
+
+int main() {
+    cout << myth << endl;
+    cout << myNum;
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y, sum;
+
+    cout << "Type a number: ";
+    cin >> x;
+
+    cout << "Type another number: ";
+    cin >> y;
+
+    sum = x + y;
+
+    cout << "Sum: " << sum;
+
+    return 0;
+}
+#include<iostream>
+using namespace std;
+float fl=35e3;
+double dl=12E4;
+int main(){
+cout<<fl;
+cout<<dl;
+return 0;
